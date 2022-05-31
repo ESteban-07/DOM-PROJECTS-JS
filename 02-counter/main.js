0883ceb844f07a19868a9cc66bf6a321a4013acd
Esteban Portillo <esteban.07.elias@gmail.com>
2022-05-31 00:02:08 -0600

    project finished

--- 02-counter/main.js
+++ 02-counter/main.js
@@ -1,22 +1,44 @@
+// Variables
 let counter = 0;
-const paragraph = document.getElementById('demo');
-const btnDecrease = document.getElementById('btn-1');
-const btnReset = document.getElementById('btn-2');
-const btnIncrease = document.getElementById('btn-3');
+const paragraph = document.getElementById("demo");
+const btnDecrease = document.getElementById("btn-1");
+const btnReset = document.getElementById("btn-2");
+const btnIncrease = document.getElementById("btn-3");
 
+// Event Listeners
 btnDecrease.addEventListener("click", decreaseCounter);
-btnReset.addEventListener("click", resetCounter)
+btnReset.addEventListener("click", resetCounter);
 btnIncrease.addEventListener("click", increaseCounter);
 
+// Functions
 function decreaseCounter() {
-    return paragraph.innerHTML = --counter;
+    --counter;
+    colorCounter(counter);
+    return (paragraph.innerHTML = counter);
 }
 
 function resetCounter() {
     counter = 0;
-    return paragraph.innerHTML = counter;
+    colorCounter(counter);
+    return (paragraph.innerHTML = counter);
 }
 
 function increaseCounter() {
-    return paragraph.innerHTML = ++counter;
+    ++counter;
+    colorCounter(counter);
+    return (paragraph.innerHTML = counter);
+}
+
+function colorCounter(value) {
+    let result;
+    if (value > 0) {
+        result = paragraph.style.color = "green";
+    }
+    if (value < 0) {
+        result = paragraph.style.color = "red";
+    }
+    if (value == 0) {
+        result = paragraph.style.color = "#222222";
+    }
+    return result;
 }
